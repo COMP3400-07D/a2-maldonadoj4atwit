797f55f8c1ee4cdb52a021196242f1718b2858ad
maldonadoj4atwit <maldonadoj4@wit.edu>
2025-10-06 11:32:18 -0400

    Completed array_fill

--- array_fill.c
+++ array_fill.c
@@ -4,5 +4,15 @@
  * TODO: Describe what the function does
  */
 int array_fill(int* array, int array_len, int begin, int end) {
-    return 1;
+    if ( array == NULL || array_len <= 0 ) {
+        return 0 ;
+    }
+
+    int count = 0 ;
+    for ( int i = begin ; i <= end && count < array_len ; i++ ) {
+        array[ count ] = i ;
+        count++ ;
+    }
+
+    return count ;
 }
