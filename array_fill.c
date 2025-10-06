87fa1f313208d2c6ad9cb95af94facc5ba335365
maldonadoj4atwit <maldonadoj4@wit.edu>
2025-10-06 11:36:00 -0400

    Completed array_fill

--- array_fill.c
+++ array_fill.c
@@ -1,7 +1,14 @@
 #include <stddef.h> // For NULL
 
 /**
- * TODO: Describe what the function does
+ * Fills an array with consecutive integers from begin to end in increasing order,
+ * stopping early if the array runs out of space.
+ * 
+ * @param array Pointer to the array to fill
+ * @param array_len Maximum number of elements to fill
+ * @param begin Beginning integer
+ * @param end Ending integer 
+ * @return Number of elements actually filled
  */
 int array_fill(int* array, int array_len, int begin, int end) {
     if ( array == NULL || array_len <= 0 ) {
